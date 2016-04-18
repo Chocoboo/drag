1c3668d2b2969ef1f2a2422c341f1787372d5686
Chocoboo <musiq@live.cn>
2016-04-18 11:20:26 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -4,8 +4,9 @@ function darg(){
 	var oDiv = document.getElementById('div1');
 	var disX =0
 	var disY =0
-	obj.onmousedown = function(){
-		
+	obj.onmousedown = function(ev){
+		disX = ev.pageX - obj.offsetLeft;
+		disY = ev.pageY - obj.offsetTop;
 	}
 }
 
